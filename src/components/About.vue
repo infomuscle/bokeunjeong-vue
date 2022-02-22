5b9ca5480b63d9e2756c4c93dabc00211a411d8e
infomuscle <infomuscle10@gmail.com>
2022-02-22 22:04:31 +0900

    Applied Vue Templates to About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,54 @@
 <template>
-  <p>about</p>
-  <ul>
-    <li v-for="link in links">
-      {{ link }}
-    </li>
-  </ul>
-  <ul>
-    <li v-for="introduction in introductions">
-      {{ introduction }}
-    </li>
-  </ul>
+  <section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>Hello, I'm Bokeun Aaron Jeong</p>
+        <p>infomuscle10@gmail.com</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="img/profile-img.jpeg" class="img-fluid" alt="">
+          <div class="text-center my-3">
+            <a href="resume_bokeunjeong.pdf" download>
+              <button id="button-resume">Resume</button>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-8 pt-1 pt-lg-0 content">
+          <h3>Web Backend Developer.</h3>
+          <p class="fst-italic">
+            Responsible for Development of Order and Claim Service to Sync with Partner Companies
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li v-for="link in even(links)">
+                  <i class="bi bi-chevron-right"></i> <strong>{{ link.title }}:</strong>
+                  <span><a v-bind:href="link.url" target="_blank">{{ link.url }}</a></span>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li v-for="link in odd(links)">
+                  <i class="bi bi-chevron-right"></i> <strong>{{ link.title }}:</strong>
+                  <span><a v-bind:href="link.url" target="_blank">{{ link.url }}</a></span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p></p>
+          <p v-for="introduction in introductions">
+            <strong>{{ introduction.title }}</strong><br>
+            {{ introduction.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -18,6 +57,14 @@ export default {
   props: {
     links: Object,
     introductions: Object
+  },
+  methods: {
+    even(items) {
+      return items.filter((item, i) => i % 2 === 0)
+    },
+    odd(items) {
+      return items.filter((item, i) => i % 2 === 1)
+    }
   }
 }
 </script>
