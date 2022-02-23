9dd602d03e948830f628315f079cbfef87836dfa
infomuscle <infomuscle10@gmail.com>
2022-02-24 01:01:36 +0900

    arraanged wrapped

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -5,10 +5,8 @@
       <p>I'm <span class="typed" data-typed-items="Web Backend Developer"></span></p>
       <!--      <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>-->
       <div class="social-links">
-        <a href="https://www.instagram.com/i.m.archive" class="instagram" target="_blank"><i
-            class="bx bxl-instagram"></i></a>
-        <a href="https://www.linkedin.com/in/infomuscle10" class="linkedin" target="_blank"><i
-            class="bx bxl-linkedin"></i></a>
+        <a href="https://www.instagram.com/i.m.archive" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/infomuscle10" class="linkedin" target="_blank"><i class="bx bxl-linkedin"></i></a>
         <a href="https://github.com/infomuscle" class="github" target="_blank"><i class="bx bxl-github"></i></a>
       </div>
     </div>
