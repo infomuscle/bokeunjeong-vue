74e563c43e1d69b6b44b967d6a7684eb4e4d32e9
infomuscle <infomuscle10@gmail.com>
2023-08-15 15:49:47 +0900

    메일 발송 결과 메시지 표시 개선

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -22,23 +22,23 @@
           <form v-on:submit.prevent="send" role="form" class="email-form">
             <div class="row">
               <div class="col-md-6 form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" v-model="senderName" required>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name *" v-model="senderName">
               </div>
               <div class="col-md-6 form-group mt-3 mt-md-0">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" v-model="senderEmail" required>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email * " v-model="senderEmail">
               </div>
             </div>
             <div class="form-group mt-3">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" v-model="emailSubject" required>
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject *" v-model="emailSubject">
             </div>
             <div class="form-group mt-3">
-              <textarea class="form-control" name="message" rows="5" placeholder="Message" v-model="emailMessage" required></textarea>
+              <textarea class="form-control" name="message" rows="5" placeholder="Message *" v-model="emailMessage"></textarea>
             </div>
             <div class="my-3">
               <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <!--                        <div class="sent-message">Your message has been sent. Thank you!</div>-->
-              <div class="sent-message">On Construct</div>
+              <div class="error-message">Failed to Send message. Please contact via email address left</div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+              <!--              <div class="sent-message">On Construct</div>-->
             </div>
             <div class="text-center">
               <button type="submit">Send Message</button>
@@ -81,9 +81,30 @@ export default {
       const response = axios.post('/api/v1/portfolio/email', request).then(response => {
         console.log(response.data);
 
-        document.querySelector('.sent-message').classList.add('d-block');
-        // thisForm.reset();
-      });
+        let errorMessage = document.querySelector('.error-message');
+        if (errorMessage.classList.contains('d-block')) {
+          errorMessage.classList.remove('d-block');
+        }
+
+        let sentMessage = document.querySelector('.sent-message');
+        if (!sentMessage.classList.contains('d-block')) {
+          sentMessage.classList.add('d-block');
+        }
+      }).catch(
+          response => {
+            console.log(response.data);
+
+            let sentMessage = document.querySelector('.sent-message');
+            if (sentMessage.classList.contains('d-block')) {
+              sentMessage.classList.remove('d-block');
+            }
+
+            let errorMessage = document.querySelector('.error-message');
+            if (!errorMessage.classList.contains('d-block')) {
+              errorMessage.classList.add('d-block');
+            }
+          }
+      );
 
       this.clear();
     },
